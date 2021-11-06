b1d55b25bea6923dd370e59fabac509674c65439
Pig Fang <g-plane@hotmail.com>
2021-11-06 15:16:54 +0800

    refactor build script

--- build.js
+++ build.js
@@ -3,28 +3,23 @@ const path = require('path')
 const browserslist = require('browserslist')
 const e2c = require('electron-to-chromium/versions')
 
-async function main() {
-  await fs.writeFile(
+Promise.all([
+  fs.writeFile(
     path.join(process.env.OUT_DIR, 'caniuse-lite-browsers.json'),
     JSON.stringify(browserslist.data)
-  )
-
-  await fs.writeFile(
+  ),
+  fs.writeFile(
     path.join(process.env.OUT_DIR, 'caniuse-lite-usage.json'),
     JSON.stringify(browserslist.usage.global)
-  )
-
-  await fs.writeFile(
+  ),
+  fs.writeFile(
     path.join(process.env.OUT_DIR, 'caniuse-lite-version-aliases.json'),
     JSON.stringify(browserslist.versionAliases)
-  )
-
-  await fs.writeFile(
+  ),
+  fs.writeFile(
     path.join(process.env.OUT_DIR, 'electron-to-chromium.json'),
     JSON.stringify(
       Object.entries(e2c).map(([k, v]) => [Number.parseFloat(k), v])
     )
-  )
-}
-
-main()
+  ),
+])
