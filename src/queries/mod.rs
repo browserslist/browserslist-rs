27f679ede7bf2fbfc90b4d5b3dd0e3218f38ddce
Pig Fang <g-plane@hotmail.com>
2021-11-06 10:27:42 +0800

    support simple percentage query

--- src/queries/mod.rs
+++ src/queries/mod.rs
@@ -4,21 +4,23 @@ mod electron;
 mod firefox_esr;
 mod last_electron;
 mod last_n_versions;
+mod percentage;
 
 trait Selector {
     fn select(&self, text: &str) -> Option<Vec<String>>;
 }
 
 pub fn query(query_string: &str) -> Option<Vec<String>> {
-    let queries: Vec<Box<dyn Selector>> = vec![
+    let selectors: Vec<Box<dyn Selector>> = vec![
         Box::new(last_n_versions::LastNVersionsSelector),
+        Box::new(percentage::Percentage),
         Box::new(last_electron::LastElectronSelector),
         Box::new(electron::ElectronSelector),
         Box::new(firefox_esr::FirefoxESRSelector),
         Box::new(dead::DeadSelector),
     ];
 
-    queries
+    selectors
         .into_iter()
         .find_map(|selector| selector.select(query_string))
 }
---
+++ src/queries/percentage.rs
@@ -0,0 +1,27 @@
+use super::{caniuse::CANIUSE_LITE_USAGE, Selector};
+use once_cell::sync::Lazy;
+use regex::Regex;
+
+static REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r"(>=?|<=?)\s*(\d+|\d+\.\d+|\.\d+)%").unwrap());
+
+pub(super) struct Percentage;
+
+impl Selector for Percentage {
+    fn select(&self, text: &str) -> Option<Vec<String>> {
+        let matches = REGEX.captures(text)?;
+        let sign = matches.get(1)?.as_str();
+        let popularity: f32 = matches.get(2)?.as_str().parse().ok()?;
+
+        let versions = CANIUSE_LITE_USAGE
+            .iter()
+            .filter(|(_, usage)| match sign {
+                ">" => **usage > popularity,
+                "<" => **usage < popularity,
+                "<=" => **usage <= popularity,
+                _ => **usage >= popularity,
+            })
+            .map(|(version, _)| version.clone())
+            .collect();
+        Some(versions)
+    }
+}
