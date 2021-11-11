3b0984efbaf228a0e4c9a42db12441f69f16cf4d
Pig Fang <g-plane@hotmail.com>
2021-11-11 18:26:22 +0800

    add crate example

---
+++ examples/inspect.rs
@@ -0,0 +1,34 @@
+use browserslist::{resolve, Opts};
+use clap::{App, Arg};
+
+fn main() {
+    let matches = App::new("Browserslist")
+        .arg(
+            Arg::with_name("mobile_to_desktop")
+                .long("mobile-to-desktop")
+                .takes_value(false),
+        )
+        .arg(
+            Arg::with_name("ignore_unknown_versions")
+                .long("ignore-unknown-versions")
+                .takes_value(false),
+        )
+        .arg(Arg::with_name("queries"))
+        .get_matches();
+
+    let opts = Opts::new()
+        .mobile_to_desktop(matches.is_present("mobile_to_desktop"))
+        .ignore_unknown_versions(matches.is_present("ignore_unknown_versions"));
+
+    match resolve(
+        &vec![matches.value_of("queries").unwrap_or_default()],
+        &opts,
+    ) {
+        Ok(versions) => {
+            for version in versions {
+                println!("{}", version)
+            }
+        }
+        Err(error) => eprintln!("{}", error),
+    };
+}
--- src/main.rs
+++
@@ -1,8 +0,0 @@
-use browserslist::{resolve, Opts};
-use std::env;
-
-fn main() {
-    for item in resolve(&env::args().skip(1).collect::<Vec<_>>(), &Opts::default()).unwrap() {
-        println!("{}", item);
-    }
-}
