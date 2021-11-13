43d2ac8f7da25d1604791110a0be28ad01109f44
Pig Fang <g-plane@hotmail.com>
2021-11-13 09:44:35 +0800

    fix `defaults` query

--- src/queries/defaults.rs
+++ src/queries/defaults.rs
@@ -1,19 +1,16 @@
-use super::{query, Selector, SelectorResult};
-use crate::{error::Error, opts::Opts};
+use super::{Selector, SelectorResult};
+use crate::{opts::Opts, resolve};
 
 pub(super) struct DefaultsSelector;
 
 impl Selector for DefaultsSelector {
     fn select<'a>(&self, text: &'a str, opts: &Opts) -> SelectorResult {
         if text.eq_ignore_ascii_case("defaults") {
-            ["> 0.5%", "last 2 versions", "Firefox ESR", "not dead"]
-                .into_iter()
-                .map(|q| query(q, opts))
-                .try_fold(Vec::with_capacity(20), |mut result, current| {
-                    result.append(&mut current?);
-                    Ok::<_, Error>(result)
-                })
-                .map(Some)
+            resolve(
+                ["> 0.5%", "last 2 versions", "Firefox ESR", "not dead"],
+                opts,
+            )
+            .map(Some)
         } else {
             Ok(None)
         }
