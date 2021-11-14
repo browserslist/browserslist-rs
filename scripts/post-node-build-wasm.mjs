d2286bcd27fdd1ba621120be72ba18c825cf7e26
Pig Fang <g-plane@hotmail.com>
2021-11-14 18:01:51 +0800

    rename WASM build target

--- scripts/post-node-build-wasm.mjs
+++ scripts/post-node-build-wasm.mjs
@@ -2,17 +2,16 @@ import * as fs from 'node:fs/promises'
 
 Promise.all([
   fs.writeFile(
-    './pkg/esm/index.js',
-    `export * from './browserslist.js';\nexport { resolveToStrings as default } from './browserslist.js';`
+    './pkg/node/index.js',
+    `module.exports = require('./browserslist').resolveToStrings`
   ),
   (async () => {
-    const manifestPath = './pkg/esm/package.json'
+    const manifestPath = './pkg/node/package.json'
     const pkg = JSON.parse(await fs.readFile(manifestPath, 'utf8'))
 
     pkg.author = pkg.collaborators[0]
     pkg.files.push('index.js')
     pkg.main = 'index.js'
-    pkg.module = 'index.js'
 
     await fs.writeFile(manifestPath, JSON.stringify(pkg, null, 2))
   })(),
--- scripts/post-web-build-wasm.mjs
+++ scripts/post-web-build-wasm.mjs
@@ -2,16 +2,17 @@ import * as fs from 'node:fs/promises'
 
 Promise.all([
   fs.writeFile(
-    './pkg/cjs/index.js',
-    `module.exports = require('./browserslist').resolveToStrings`
+    './pkg/web/index.js',
+    `export * from './browserslist.js';\nexport { resolveToStrings as default } from './browserslist.js';`
   ),
   (async () => {
-    const manifestPath = './pkg/cjs/package.json'
+    const manifestPath = './pkg/web/package.json'
     const pkg = JSON.parse(await fs.readFile(manifestPath, 'utf8'))
 
     pkg.author = pkg.collaborators[0]
     pkg.files.push('index.js')
     pkg.main = 'index.js'
+    pkg.module = 'index.js'
 
     await fs.writeFile(manifestPath, JSON.stringify(pkg, null, 2))
   })(),
