6300d5c6d1a3c597190ae47d986c230afb1f29df
Pig Fang <g-plane@hotmail.com>
2021-11-14 15:32:20 +0800

    support `current node` on WASM

--- src/queries/current_node.rs
+++ src/queries/current_node.rs
@@ -1,4 +1,4 @@
-use super::{Selector, SelectorResult};
+use super::{Distrib, Selector, SelectorResult};
 use crate::opts::Opts;
 use once_cell::sync::Lazy;
 use regex::{Regex, RegexBuilder};
@@ -17,12 +17,22 @@ impl Selector for CurrentNodeSelector {
         if REGEX.is_match(text) {
             #[cfg(target_arch = "wasm32")]
             {
-                Err(crate::error::Error::UnsupportedCurrentNode)
+                use crate::error::Error;
+                use js_sys::{global, Reflect};
+
+                let obj_process = Reflect::get(&global(), &"process".into())
+                    .map_err(|_| Error::UnsupportedCurrentNode)?;
+                let obj_versions = Reflect::get(&obj_process, &"versions".into())
+                    .map_err(|_| Error::UnsupportedCurrentNode)?;
+                let version = Reflect::get(&obj_versions, &"node".into())
+                    .map_err(|_| Error::UnsupportedCurrentNode)?
+                    .as_string()
+                    .ok_or(Error::UnsupportedCurrentNode)?;
+                Ok(Some(vec![Distrib::new("node", version)]))
             }
 
             #[cfg(not(target_arch = "wasm32"))]
             {
-                use super::Distrib;
                 use std::process::Command;
 
                 let output = Command::new("node").arg("-v").output().unwrap();
