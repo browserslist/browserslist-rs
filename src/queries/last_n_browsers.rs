74ac154739870140e30202129b17537815b344e2
Pig Fang <g-plane@hotmail.com>
2021-11-15 09:41:53 +0800

    add more tests

--- src/queries/last_n_browsers.rs
+++ src/queries/last_n_browsers.rs
@@ -45,3 +45,18 @@ impl Selector for LastNBrowsersSelector {
         Ok(Some(versions))
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::test::run_compare;
+    use test_case::test_case;
+
+    #[test_case("last 2 versions"; "basic")]
+    #[test_case("last 31 versions"; "android")]
+    #[test_case("last 1 version"; "support pluralization")]
+    #[test_case("Last 02 Versions"; "case insensitive")]
+    fn valid(query: &str) {
+        run_compare(query, &Opts::new());
+    }
+}
