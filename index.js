8ca09f378333fa6be346710ed22592d7ac6a7639
Pig Fang <g-plane@hotmail.com>
2021-11-17 12:10:48 +0800

    tweak entry

--- index.js
+++
@@ -1,5 +0,0 @@
-const { loadBinding } = require('@node-rs/helper')
-
-const bindings = loadBinding(__dirname, 'browserslist-rs', 'browserslist-rs')
-
-module.exports = bindings.execute
---
+++ index.ts
@@ -0,0 +1,16 @@
+import { loadBinding } from '@node-rs/helper'
+
+const bindings = loadBinding(__dirname, 'browserslist-rs', 'browserslist-rs')
+
+interface Opts {
+  mobileToDesktop: boolean
+  ignoreUnknownVersions: boolean
+}
+
+interface Execute {
+  (query: string | string[], opts?: Opts): string[]
+}
+
+const execute: Execute = bindings.execute
+
+export = execute
