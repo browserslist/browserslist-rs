eb1729969b5fa442ac0e7a405657765fb9760066
Pig Fang <g-plane@hotmail.com>
2021-11-18 09:25:59 +0800

    add more tests

--- src/parser.rs
+++ src/parser.rs
@@ -31,3 +31,19 @@ pub fn parse(query: &str) -> impl Iterator<Item = Query> {
         })
     })
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::{opts::Opts, test::run_compare};
+    use test_case::test_case;
+
+    #[test_case("ie >= 6, ie <= 7"; "comma")]
+    #[test_case("ie >= 6 and ie <= 7"; "and")]
+    #[test_case("ie < 11 and not ie 7"; "and with not")]
+    #[test_case("last 1 Baidu version and not <2%"; "with not and one-version browsers as and query")]
+    #[test_case("ie >= 6 or ie <= 7"; "or")]
+    #[test_case("ie < 11 or not ie 7"; "or with not")]
+    fn valid(query: &str) {
+        run_compare(query, &Opts::new());
+    }
+}
