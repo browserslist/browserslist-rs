7ebfe39b3e9977afb6ce08122545421f7ac855bd
Pig Fang <g-plane@hotmail.com>
2021-11-23 17:09:26 +0800

    download "node-releases" data in build script

--- build.rs
+++ build.rs
@@ -1,7 +1,12 @@
 use anyhow::Result;
-use std::{collections::BTreeMap, env, fs};
+use serde::Deserialize;
+use std::{
+    collections::{BTreeMap, HashMap},
+    env, fs,
+};
 
 const E2C: &str = "1.3.906";
+const NODE: &str = "2.0.1";
 
 fn main() -> Result<()> {
     #[cfg(feature = "node")]
@@ -12,6 +17,8 @@ fn main() -> Result<()> {
     println!("cargo:rerun-if-changed=build.rs");
 
     fetch_electron_to_chromium()?;
+    fetch_node_versions()?;
+    fetch_node_release_schedule()?;
 
     Ok(())
 }
@@ -39,6 +46,64 @@ fn fetch_electron_to_chromium() -> Result<()> {
     fs::write(
         format!("{}/electron-to-chromium.json", env::var("OUT_DIR")?),
         &serde_json::to_string(&data)?,
+    )?;
+
+    Ok(())
+}
+
+fn fetch_node_versions() -> Result<()> {
+    #[derive(Deserialize)]
+    struct NodeRelease {
+        version: String,
+    }
+
+    let releases = ureq::get(&format!(
+        "https://cdn.jsdelivr.net/npm/node-releases@{}/data/processed/envs.json",
+        NODE
+    ))
+    .call()?
+    .into_json::<Vec<NodeRelease>>()?;
+
+    fs::write(
+        format!("{}/node-versions.json", env::var("OUT_DIR")?),
+        &serde_json::to_string(
+            &releases
+                .into_iter()
+                .map(|release| release.version)
+                .collect::<Vec<_>>(),
+        )?,
+    )?;
+
+    Ok(())
+}
+
+fn fetch_node_release_schedule() -> Result<()> {
+    #[derive(Deserialize)]
+    struct NodeRelease {
+        start: String,
+        end: String,
+    }
+
+    let schedule = ureq::get(&format!(
+        "https://cdn.jsdelivr.net/npm/node-releases@{}/data/release-schedule/release-schedule.json",
+        NODE
+    ))
+    .call()?
+    .into_json::<HashMap<String, NodeRelease>>()?;
+
+    fs::write(
+        format!("{}/node-release-schedule.json", env::var("OUT_DIR")?),
+        &serde_json::to_string(
+            &schedule
+                .into_iter()
+                .map(|(version, release)| {
+                    (
+                        version.trim_start_matches('v').to_owned(),
+                        (release.start, release.end),
+                    )
+                })
+                .collect::<HashMap<_, _>>(),
+        )?,
     )?;
 
     Ok(())
--- scripts/sync.ts
+++ scripts/sync.ts
@@ -2,8 +2,6 @@ import * as fs from 'node:fs/promises'
 import * as path from 'node:path'
 import * as process from 'node:process'
 import browserslist from 'browserslist'
-import nodeVersions from 'node-releases/data/processed/envs.json'
-import nodeReleaseSchedule from 'node-releases/data/release-schedule/release-schedule.json'
 
 const dest = path.join(process.cwd(), 'data')
 
@@ -26,20 +24,5 @@ Promise.all([
   fs.writeFile(
     path.join(dest, 'caniuse-lite-version-aliases.json'),
     JSON.stringify(browserslist.versionAliases)
-  ),
-  fs.writeFile(
-    path.join(dest, 'node-versions.json'),
-    JSON.stringify(nodeVersions.map(({ version }) => version))
-  ),
-  fs.writeFile(
-    path.join(dest, 'node-release-schedule.json'),
-    JSON.stringify(
-      Object.fromEntries(
-        Object.entries(nodeReleaseSchedule).map(([version, { start, end }]) => [
-          version.slice(1),
-          [start, end],
-        ])
-      )
-    )
   ),
 ])
